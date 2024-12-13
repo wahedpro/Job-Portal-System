3c411f76172fee3ba28cbbd7f456867814608a46
Wahedul Islam <wahedulislam.pro@gmail.com>
2024-12-13 19:55:16 +0600

    add navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,39 @@
 const Footer = () => {
     return (
         <div>
-            <h1>This is footer section</h1>
+            <div className="flex justify-between pb-5">
+                <div  className="flex flex-col">
+                    <h1 className="text-2xl font-semibold">Job Portal Website</h1>
+                    <p>Find the new job.</p>
+                </div>
+                <div  className="flex flex-col">
+                    <h1 className="text-xl font-semibold">Company</h1>
+                    <a href="">About Us</a>
+                    <a href="">Services</a>
+                    <a href="">Community</a>
+                    <a href="">Testimonials</a>
+                </div>
+                <div  className="flex flex-col">
+                    <h1 className="text-xl font-semibold">Support</h1>
+                    <a href="">Help Center</a>
+                    <a href="">FeedBack</a>
+                    <a href="">Webinars</a>
+                    <a href="">Testimonials</a>
+                </div>
+                <div  className="flex flex-col">
+                    <h1 className="text-xl font-semibold">Links</h1>
+                    <a href="">Courses</a>
+                    <a href="">Become Teacher</a>
+                    <a href="">Service</a>
+                    <a href="">All in One</a>
+                </div>
+                <div  className="flex flex-col">
+                    <h1 className="text-xl font-semibold">Contact Us</h1>
+                    <a href="">01863377256</a>
+                    <a href="">wahedulislam.pro@gamial.com</a>
+                </div>
+            </div>
+            <p className="text-center pt-5 border-t">Copyright @ 2024 Job portal. All rights reserved</p>
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is navbar</h1>
+        <div className="flex justify-between items-center py-3">
+            <NavLink to='/'><h1 className="text-2xl font-semibold">Job Portal</h1></NavLink>
+            <div className="flex gap-3">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/'>All Jobs</NavLink>
+                <NavLink to='/'>Add Jobs</NavLink>
+                <NavLink to='/'>Application Page</NavLink>
+                <NavLink to='/'>My Applications</NavLink>
+                <NavLink to='/'>My JobPost</NavLink>
+            </div>
+            <div className="flex gap-2">
+                <NavLink><button className="px-5 py-2 border">Login</button></NavLink>
+                <NavLink><button className="px-5 py-2 border">Register</button></NavLink>
+            </div>
         </div>
     );
 };
